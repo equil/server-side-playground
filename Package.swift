1a8090091db5eda8ea4a58faa462f1bbe682e1ab
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2017-05-03 01:24:02 +0300

    remove dependency

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
     name: "Twoot",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/zmeyc/telegram-bot-swift.git", majorVersion: 0),
-        .Package(url: "https://github.com/mattdonnelly/Swifter", majorVersion: 0)
+        .Package(url: "https://github.com/zmeyc/telegram-bot-swift.git", majorVersion: 0)
     ],
     exclude: [
         "Config",
