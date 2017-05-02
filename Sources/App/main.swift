5dfa8a155516a7d9d5bcf6924ba8b10bb5dd06b1
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2017-05-03 02:00:22 +0300

    testing

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,5 @@
 import Vapor
+import HTTP
 
 let drop = Droplet()
 
@@ -6,6 +7,16 @@ drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+}
+
+drop.post("/system/git") { request in
+    guard let bytes = request.body.bytes else {
+        throw Abort.badRequest
+    }
+    let json = try JSON(bytes: bytes)
+    print(json)
+    let response = Response(status: .ok, body: "")
+    return response
 }
 
 drop.resource("posts", PostController())
