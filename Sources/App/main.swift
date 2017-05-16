d2858ddd98b972c117f9f63add79fe273168baf8
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2017-05-17 02:32:35 +0300

    no message

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,9 +3,12 @@ import HTTP
 import Foundation
 import Console
 
+// Modules configuration
 ModulesFather.register(WelcomeModule.self)
 ModulesFather.register(ContiniousDeliveryModule.self)
 
+
+// Server startup
 let drop = Droplet()
 try drop.addProvider(ModulesFather.self)
 
