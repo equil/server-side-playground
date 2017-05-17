fd70b1131de280c30d1cf19dcf6d5324d187ee82
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2017-05-17 03:03:28 +0300

    no message

--- Sources/App/ContiniousDeliveryModule.swift
+++ Sources/App/ContiniousDeliveryModule.swift
@@ -36,6 +36,7 @@ public final class ContiniousDeliveryModule : PlaygroundModule {
                 task.currentDirectoryPath = "/home/ubuntu"
                 task.arguments = ["./redeploy.sh"]
                 task.launch()
+                print(task.terminationReason)
                 response = Response(status: .created, body: "{}")
             } else {
                 response = Response(status: .notModified, body: "{}")
