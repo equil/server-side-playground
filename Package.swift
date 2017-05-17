fcb5c2f780aee9c609e2a378fe9937cd445d81f9
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2017-05-17 17:40:35 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ServerSidePlayground",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minorVersion: 5),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/zmeyc/telegram-bot-swift.git", majorVersion: 0)
     ],
     exclude: [
